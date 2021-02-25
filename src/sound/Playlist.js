729712eb316ef78a4692c481bedab8c0ec87ae30
Andrey <a.prudniks@gmail.com>
2021-02-25 13:04:07 +0200

    new sounds

--- src/sound/Playlist.js
+++ src/sound/Playlist.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import click from './click.wav';
-import money from './money.mp3';
-import bell from './bell.mp3';
-import tic from './tic.mp3';
-import clack from './clack.mp3';
+// import click from './click.wav';
+// import money from './money.mp3';
+// import bell from './bell.mp3';
+// import tic from './tic.mp3';
+// import clack from './clack.mp3';
 import fireworks from './fireworks.mp3';
 import Sound from "./Sound";
 
 const Playlist = () => {
     return (
         <div>
-            <Sound param={{id: 'click', effect: click}}/>
-            <Sound param={{id: 'clack', effect: clack}}/>
-            <Sound param={{id: 'money', effect: money}}/>
-            <Sound param={{id: 'bell', effect: bell}}/>
-            <Sound param={{id: 'tic', effect: tic}}/>
+            {/*<Sound param={{id: 'click', effect: click}}/>*/}
+            {/*<Sound param={{id: 'clack', effect: clack}}/>*/}
+            {/*<Sound param={{id: 'money', effect: money}}/>*/}
+            {/*<Sound param={{id: 'bell', effect: bell}}/>*/}
+            {/*<Sound param={{id: 'tic', effect: tic}}/>*/}
             <Sound param={{id: 'fireworks', effect: fireworks}}/>
         </div>
     );
