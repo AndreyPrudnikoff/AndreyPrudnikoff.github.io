05e24ec0845b37bb2eddb01f0e0c6d6cea1540be
kirilloidas <peregudakirill90@gmail.com>
2021-03-01 14:45:20 +0200

    intro adaptive

--- src/components/Intro/Step1.js
+++ src/components/Intro/Step1.js
@@ -6,44 +6,82 @@ import {EN} from '../../languages/en'
 import {RU} from '../../languages/ru'
 import {connect} from 'react-redux'
 
-const Step1 = ({switchStep, playClick, currentLang}) => {
+const Step1 = ({switchStep, playClick, currentLang, widthMode}) => {
     const LANG = currentLang === 'en' ? EN : RU;
-    return (
-        <div className="step step1">
-            <img className='step-img' src={imgPerson} alt='person'/>
-            <div className='step-content'>
-                <ul className='list-lines'>
-                    <li className="list-lines__item" />
-                    <li className="list-lines__item" />
-                    <li className="list-lines__item" />
-                    <li className="list-lines__item" />
-                    <li className="list-lines__item" />
-                </ul>
-                <h3 className='step-content__title'>{LANG.Intro.Step1.title}</h3>
-                <p className='step-content__content'>{LANG.Intro.Step1.content}</p>
-                <ul className="step-nav">
-                    <li className='step-nav__item step-nav_btnSkip' onClick={() => {
-                        switchStep(0);
-                        playClick()
-                    }}>
-                        <span>{LANG.Intro.btnIntro.skip}</span>
-                    </li>
-                    <li className='step-nav__item' onClick={()=>{
-                        switchStep(2);
-                        playClick()
-                    }}>
-                        <span>{LANG.Intro.btnIntro.next}</span>
-                    </li>
-                </ul>
+    const isDesktop = widthMode === "desktop" ? true : false;
+    console.log(isDesktop)
+    if(isDesktop) {
+        return (
+            <div className="step step1">
+                <img className='step-img' src={imgPerson} alt='person'/>
+                <div className='step-content'>
+                    <ul className='list-lines'>
+                        <li className="list-lines__item" />
+                        <li className="list-lines__item" />
+                        <li className="list-lines__item" />
+                        <li className="list-lines__item" />
+                        <li className="list-lines__item" />
+                    </ul>
+                    <h3 className='step-content__title'>{LANG.Intro.Step1.title}</h3>
+                    <p className='step-content__content'>{LANG.Intro.Step1.content}</p>
+                    <ul className="step-nav">
+                        <li className='step-nav__item step-nav_btnSkip' onClick={() => {
+                            switchStep(0);
+                            playClick()
+                        }}>
+                            <span>{LANG.Intro.btnIntro.skip}</span>
+                        </li>
+                        <li className='step-nav__item' onClick={()=>{
+                            switchStep(2);
+                            playClick()
+                        }}>
+                            <span>{LANG.Intro.btnIntro.next}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>       
+        )    
+    } else {
+        return (
+            <div className="step step1">
+                <div className='top-content'>
+                    <img className='step-img' src={imgPerson} alt='person'/>
+                        <ul className='list-lines'>
+                            <li className="list-lines__item" />
+                            <li className="list-lines__item" />
+                            <li className="list-lines__item" />
+                            <li className="list-lines__item" />
+                            <li className="list-lines__item" />
+                        </ul>
+                </div>
+                <div className='step-content'>
+                    <h3 className='step-content__title'>{LANG.Intro.Step1.title}</h3>
+                    <p className='step-content__content'>{LANG.Intro.Step1.content}</p>
+                    <ul className="step-nav">
+                        <li className='step-nav__item step-nav_btnSkip' onClick={() => {
+                            switchStep(0);
+                            playClick()
+                        }}>
+                            <span>{LANG.Intro.btnIntro.skip}</span>
+                        </li>
+                        <li className='step-nav__item' onClick={()=>{
+                            switchStep(2);
+                            playClick()
+                        }}>
+                            <span>{LANG.Intro.btnIntro.next}</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-            
-    )
+        )
+    }
+    
 }
 
 const mapStateToProps = state => {
     return {
-        currentLang: state.switchOptions.lang
+        currentLang: state.switchOptions.lang,
+        widthMode: state.switchOptions.widthMode
     } 
 }
 const mapDispatchToProps = {
