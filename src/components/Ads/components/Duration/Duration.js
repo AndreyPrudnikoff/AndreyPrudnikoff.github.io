1e1f53b86302cb5c0707b3951a048dddedc5c70f
kirilloidas <peregudakirill90@gmail.com>
2021-03-05 15:48:42 +0200

    validate date v.1.0

--- src/components/Ads/components/Duration/Duration.js
+++ src/components/Ads/components/Duration/Duration.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import dayjs from "dayjs";
 // components
 import { Tabs, TimeInput, DateInput } from "./components";
 // style
@@ -7,6 +8,25 @@ import {connect} from "react-redux";
 import {setEndDate, setEndTime, setStartDate, setStartTime} from "../../../../redux/actions/advertising";
 
 const Duration = ({setStartDate, setStartTime, setEndDate, setEndTime, startTime}) => {
+  const [dateStart, setDateStart] = useState();
+  const [timeStart, setTimeStart] = useState();
+  const [dateEnd, setDateEnd] = useState();
+  const [timeEnd, setTimeEnd] = useState();
+  const [isStartDate, setIsStartDate] = useState(true);
+  const [isEndDate, setIsEndDate] = useState(true);
+
+  useEffect(() => {
+    console.log(dateStart, timeStart, dateEnd, timeEnd)
+    const dateNow = dayjs().valueOf();
+    let enteredDate = dayjs(`${dateStart}T${timeStart}`).valueOf();
+    console.log(dayjs(`${dateStart}T${timeStart}`).valueOf(), dayjs().valueOf())
+    if(enteredDate > dateNow) {
+      setIsStartDate(true)
+    } else if (enteredDate < dateNow) {
+      setIsStartDate(false)
+    }
+  }, [dateStart, timeStart, dateEnd, timeEnd])
+
   const tabs = [
     {
       id: 0,
@@ -14,12 +34,12 @@ const Duration = ({setStartDate, setStartTime, setEndDate, setEndTime, startTime
       content: (
         <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
           <div style={{ display: "flex", gap: "95px" }}>
-            <DateInput onChange={setStartDate} label="Start date" />
-            <TimeInput onChange={setStartTime} label="Start time" />
+            <DateInput onChange={(e) => {setStartDate(e); setDateStart(e)}} label="Start date" />
+            <TimeInput onChange={(e) => {setStartTime(e); setTimeStart(e)}} label="Start time" />
           </div>
           <div style={{ display: "flex", gap: "95px" }}>
-            <DateInput onChange={setEndDate} label="End date" />
-            <TimeInput onChange={setEndTime} label="End time" />
+            <DateInput onChange={(e) => {setEndDate(e); setDateEnd(e)}} label="End date" />
+            <TimeInput onChange={(e) => {setEndTime(e); setTimeEnd(e)}} label="End time" />
           </div>
         </div>
       ),
