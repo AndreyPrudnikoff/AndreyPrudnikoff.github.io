c9e972d68c47fb33aa5b67e45d4427d06e3da7f9
Andrey <a.prudniks@gmail.com>
2021-03-11 17:10:41 +0200

    fix style listAD

--- src/components/Ads/ListAds.js
+++ src/components/Ads/ListAds.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import "./ads.scss";
 import {useHistory} from "react-router-dom";
 import {playClick} from "../../redux/actions/music";
@@ -20,6 +20,13 @@ const ListAds = ({playClick, name, balance, currentLang, currentList, finishedLi
     useEffect(() => {
         getCurrentList();
     }, [])
+    const lengthArray = 6 - currentList.length;
+    const adNew = [];
+
+    for (let i = 0; i < lengthArray; i++) {
+        adNew.push(1);
+        console.log(adNew)
+    }
 
     const getTotalTime = (startTime, startDate, endTime, endDate) => {
         const start = `${startDate} ${startTime}`;
@@ -32,74 +39,83 @@ const ListAds = ({playClick, name, balance, currentLang, currentList, finishedLi
             <div className='listBlock'>
                 <div className='round-dark listAds'>
                     <span className='backbtn-title-span'>
-                        <img src={back} alt='back' onClick={() => history.push('/game')} className='backbtn-title-span__btn'/>
+                        <img src={back} alt='back' onClick={() => history.push('/game')}
+                             className='backbtn-title-span__btn'/>
                         <h2 className='backbtn-title-span__title'>{LANG.Ads.MyAds.title}</h2>
                     </span>
-                    
+
                     <div className='images-switch'>
                         <span onClick={() => {
                             setIsCurrent(true)
                         }}
-                            className={!isCurrent ? 'images-switch__item' : 'images-switch__item active'}>{LANG.Ads.MyAds.current}</span>
+                              className={!isCurrent ? 'images-switch__item' : 'images-switch__item active'}>{LANG.Ads.MyAds.current}</span>
                         <span onClick={() => {
                             setIsCurrent(false)
                         }}
-                            className={isCurrent ? 'images-switch__item' : 'images-switch__item active'}>{LANG.Ads.MyAds.finished}</span>
+                              className={isCurrent ? 'images-switch__item' : 'images-switch__item active'}>{LANG.Ads.MyAds.finished}</span>
                     </div>
                     <ul className='images-list'>
                         {isCurrent ?
                             currentList.length
-                            ? 
-                            <React.Fragment>{currentList.map((item, index) => (
-                                <li key={index * 2} className='images-list__item' onClick={() => {
-                                    getDetails(item);
-                                    history.push('/myad')
-                                }}>
-                                    <ul className='ad-detail-list'>
-                                        <li className='ad-detail-list__item ad-detail-list__img'>
-                                            <img src={item.image} alt='image' width='259' height='245'/>
-                                        </li>
-                                        <li className='ad-detail-list__item ad-detail-list__url'>{item.website_url}</li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>{Object.keys(item.country_timezone)[0]}</span>
-                                            <span>UTC {item.country_timezone[`${Object.keys(item.country_timezone)[0]}`]}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Started</span>
-                                            <span>{item.start_date}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Finished</span>
-                                            <span>{item.end_date}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Totel time</span>
-                                            <span>{getTotalTime(item.start_time, item.start_date, item.end_time, item.end_date)}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Displays</span>
-                                            <span>{item.displays}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Clicks</span>
-                                            <span>{item.clicks}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Budget</span>
-                                            <span>
+                                ?
+                                <React.Fragment>{currentList.map((item, index) => (
+                                    <li key={index * 2} className='images-list__item' onClick={() => {
+                                        getDetails(item);
+                                        history.push('/myad')
+                                    }}>
+                                        <ul className='ad-detail-list'>
+                                            <li className='ad-detail-list__item ad-detail-list__img'>
+                                                <img src={item.image} alt='image' width='259' height='245'/>
+                                            </li>
+                                            <li className='ad-detail-list__item ad-detail-list__url'>{item.website_url}</li>
+                                            <li className='ad-detail-list__item'>
+                                                <span
+                                                    className='item-title'>{Object.keys(item.country_timezone)[0]}</span>
+                                                <span>UTC {item.country_timezone[`${Object.keys(item.country_timezone)[0]}`]}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Started</span>
+                                                <span>{item.start_date}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Finished</span>
+                                                <span>{item.end_date}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Total time</span>
+                                                <span>{getTotalTime(item.start_time, item.start_date, item.end_time, item.end_date)}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Displays</span>
+                                                <span>{item.displays}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Clicks</span>
+                                                <span>{item.clicks}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Budget</span>
+                                                <span>
                                                 {item.budget}
-                                                <span className='item-dollar'>&#36;</span>
+                                                    <span className='item-dollar'>&#36;</span>
                                             </span>
-                                        </li>
-                                    </ul>
-                                    {/* <span className='item__title item__url'>{item.website_url || "mysite.com"}</span>
+                                            </li>
+                                        </ul>
+                                        {/* <span className='item__title item__url'>{item.website_url || "mysite.com"}</span>
                                     <span className='item__date'>{item.end_date}</span> */}
-                                </li>
-                            ))} <li className='images-list__item add-new' onClick={() => history.push('/ads')}>
-                                    <img src={addNewImg} alt='add new' width='47' height='47' />
+                                    </li>
+                                ))}{console.log(adNew)} {adNew.map(() => (<li className='images-list__item add-new'
+                                                                              onClick={() => history.push('/ads')}>
+                                    <img src={addNewImg} alt='add new' width='47' height='47'/>
                                     <span className='add-new__content-text'>Add new</span>
-                                </li>
-                                </React.Fragment> : <h2 className='images-list__not-images'>{LANG.Ads.MyAds.notAds}</h2>
+                                </li>))}
+
+                                </React.Fragment>
+                                : [1, 2, 3, 4, 5, 6].map(() => (
+                                    <li className='images-list__item add-new' onClick={() => history.push('/ads')}>
+                                        <img src={addNewImg} alt='add new' width='47' height='47'/>
+                                        <span className='add-new__content-text'>Add new</span>
+                                    </li>))
                             :
                             finishedList.length
                                 ? finishedList.map((item, index) => (
@@ -108,42 +124,42 @@ const ListAds = ({playClick, name, balance, currentLang, currentList, finishedLi
                                         history.push('/myad')
                                     }}>
                                         <ul className='ad-detail-list'>
-                                        <li className='ad-detail-list__item ad-detail-list__img'>
-                                            <img src={item.image} alt='image' width='259' height='245'/>
-                                        </li>
-                                        <li className='ad-detail-list__item ad-detail-list__url'>{item.website_url}</li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>{Object.keys(item.country_timezone)[0]}</span>
-                                            <span>UTC {item.country_timezone[`${Object.keys(item.country_timezone)[0]}`]}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Started</span>
-                                            <span>{item.start_date}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Finished</span>
-                                            <span>{item.end_date}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Totel time</span>
-                                            <span></span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Displays</span>
-                                            <span>{item.displays}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Clicks</span>
-                                            <span>{item.clicks}</span>
-                                        </li>
-                                        <li className='ad-detail-list__item'>
-                                            <span className='item-title'>Budget</span>
-                                            <span>
+                                            <li className='ad-detail-list__item ad-detail-list__img'>
+                                                <img src={item.image} alt='image' width='259' height='245'/>
+                                            </li>
+                                            <li className='ad-detail-list__item ad-detail-list__url'>{item.website_url}</li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>{Object.keys(item.country_timezone)[0]}</span>
+                                                <span>UTC {item.country_timezone[`${Object.keys(item.country_timezone)[0]}`]}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Started</span>
+                                                <span>{item.start_date}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Finished</span>
+                                                <span>{item.end_date}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Total time</span>
+                                                <span></span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Displays</span>
+                                                <span>{item.displays}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Clicks</span>
+                                                <span>{item.clicks}</span>
+                                            </li>
+                                            <li className='ad-detail-list__item'>
+                                                <span className='item-title'>Budget</span>
+                                                <span>
                                                 {item.budget}
-                                                <span className='item-dollar'>&#36;</span>
+                                                    <span className='item-dollar'>&#36;</span>
                                             </span>
-                                        </li>
-                                    </ul>
+                                            </li>
+                                        </ul>
                                         {/* <span className='item__title item__url'>{item.website_url || "mysite.com"}</span>
 
                                         <span className='item__date'>{item.end_date}</span> */}
@@ -152,15 +168,14 @@ const ListAds = ({playClick, name, balance, currentLang, currentList, finishedLi
                         }
                     </ul>
                 </div>
-                <Wallet input={false}/>
+                {/*<Wallet input={false}/>*/}
             </div>
-            
+
         </div>
     )
 }
 
 const mapStateToProps = state => {
-    console.log(state.adsOptions.finishedList)
     return {
         currentLang: state.switchOptions.lang,
         balance: state.balanceReducer.balance,
